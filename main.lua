1d70d1f662075672ddc4e028cacb6b29ba412d0b
honeyp0t <Robin.Lindell@gmail.com>
2018-04-20 15:26:10 +0200

    new background

--- main.lua
+++ main.lua
@@ -1,12 +1,34 @@
-background = love.graphics.newImage('background.jpg')
+background = love.graphics.newImage('background2.png')
 tower = love.graphics.newImage('tower.png')
+bird1 = love.graphics.newImage('bird1.png');
+
+Whitebird = {}
+Whitebird.__index = Whitebird
+Whitebird.new = function(x, y) 
+    local self = {}
+    setmetatable(self, Whitebird)
+
+    self.posX = x
+    self.posY = y
+    self.image = bird1
+    local frames = {}
+
+    frames[1] = love.graphics.newQuad(0,0,33,35,bird1:getDimensions())
+    frames[2] = love.graphics.newQuad(33,0,33,35,bird1:getDimensions())
+    frames[3] = love.graphics.newQuad(66,0,33,35,bird1:getDimensions())
+
+    return self
+end
 
 function love.update(dt)
 end
+
+bird = Whitebird.new(100, 100)
 
 function love.draw(dt)
     love.graphics.clear(100, 200, 255)
 
     love.graphics.draw(background, 0, 0)
-    love.graphics.draw(tower, 500, 200)
+    love.graphics.draw(tower, 300, 170)
+    love.graphics.draw(bird.image, bird.posX, bird.posY)
 end
